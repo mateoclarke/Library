ee29b9ac7aa9dca9dd97e4fd038fac08fd3a35d3
mateoclarke <code@mateoclarke.com>
2013-11-15 10:05:13 -0600

    Added puts statements to indicated the creation of each new instance in the Library, Borrower, and Book classes.

--- library.rb
+++ library.rb
@@ -1,9 +1,11 @@
 class Library
   def initialize
     @books = []
+    puts "You have created a new Library with no books yet."
   end
 
   def books
+
   end
 
   def list_books
@@ -16,6 +18,8 @@ class Library
   end
 
   def add_book(book)
+    #add book to the library
+    puts ""
   end
 
   def check_out(user, book)
@@ -27,9 +31,12 @@ end
 
 class Borrower
   def initialize(name)
+    @name = name
+    puts "You have added a borroewer to the library named #{@name}"
   end
 
   def borrowed_books
+    # empty array for books held by borroewer
   end
 
   def name
@@ -44,5 +51,8 @@ end
 
 class Book
   def initialize(title, author)
+    @title = title
+    @author = author
+    puts "You have added a book to the library called #{title} by the author #{author}."
   end
 end
